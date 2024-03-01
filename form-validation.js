0096fd775d61d279f37ecd5b2a408d6dbedce763
Moazam05 <salmanmoazam08@gmail.com>
2024-03-01 15:24:33 +0500

    validation form

--- form-validation.js
+++ form-validation.js
@@ -2,33 +2,48 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
 
-    // Validate form fields
-    const firstName = document.getElementById("firstName").value.trim();
-    const lastName = document.getElementById("lastName").value.trim();
-    // const email = document.getElementById("email").value.trim();
-    // const phone = document.getElementById("phone").value.trim();
-    // const address = document.getElementById("address").value.trim();
+    let isFormValid = true;
 
-    if (firstName === "") {
-      document.getElementById("firstNameError").innerText =
-        "First Name required";
-      return;
-    } else {
-      document.getElementById("firstNameError").innerText = "";
+    const fields = [
+      { id: "firstName", message: "First Name required" },
+      { id: "lastName", message: "Last Name required" },
+      { id: "email", message: "Email required" },
+      { id: "phone", message: "Phone required" },
+      { id: "address", message: "Address required" },
+      { id: "propertyType", message: "Property Type required" },
+      { id: "city", message: "City required" },
+      { id: "state", message: "State required" },
+      { id: "zip", message: "Zip required" },
+      { id: "cardNumber", message: "Card Number required" },
+      { id: "currentHomeValue", message: "Home Value required" },
+      { id: "remainingBalance", message: "Remaining Balance required" },
+      { id: "cashOutAmount", message: "Cash Out Amount required" },
+    ];
+
+    fields.forEach(({ id, message }) => {
+      const value = document.getElementById(id).value.trim();
+      const errorElement = document.getElementById(`${id}Error`);
+
+      if (value === "") {
+        errorElement.innerText = message;
+        isFormValid = false;
+      } else {
+        errorElement.innerText = "";
+      }
+    });
+
+    if (isFormValid) {
+      const formData = {};
+      fields.forEach(({ id }) => {
+        formData[id] = document.getElementById(id).value.trim();
+      });
+
+      //   alert(JSON.stringify(formData));
+
+      // Submit the form
+      // form.submit();
     }
-
-    if (lastName === "") {
-      document.getElementById("lastNameError").innerText = "Last Name required";
-      return;
-    } else {
-      document.getElementById("lastNameError").innerText = "";
-    }
-
-    // Similarly, add validation for other fields like email, phone, and address
-
-    // If all validations pass, you can submit the form
-    form.submit();
   });
 });
